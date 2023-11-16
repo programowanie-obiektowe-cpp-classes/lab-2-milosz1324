b1b7539123937f281a6d84d464215bc985924d6e
milosz1324 <121822258+milosz1324@users.noreply.github.com>
2023-11-16 20:30:47 +0100

    First commit

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,43 @@
 
 #include "Resource.hpp"
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
-};
+class ResourceManager {
+private:
+    Resource* resource;
+
+public:
+    ResourceManager() : resource(new Resource()) {}
+
+    ResourceManager(const ResourceManager& other) : resource(new Resource(*(other.resource))) {}
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this == &other) {
+            return *this;
+        }
+        delete resource;
+        resource = new Resource(*(other.resource));
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other) noexcept : resource(other.resource) {
+        other.resource = nullptr;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        if (this == &other) {
+            return *this;
+        }
+        delete resource;
+        resource = other.resource;
+        other.resource = nullptr;
+        return *this;
+    }
+
+    ~ResourceManager() {
+        delete resource;
+    }
+
+    double get() const {
+        return resource->get();
+    }
+}
